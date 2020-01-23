4e74e31d23d62a354d402a417fb500da9d940f51
Kim Woelders <kim@woelders.dk>
2020-01-23 17:16:48 +0100

    bindings: Add 'W' as alias for modifier 4

--- src/aclass.c
+++ src/aclass.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2004-2019 Kim Woelders
+ * Copyright (C) 2004-2020 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -546,8 +546,6 @@ ActionDecode(const char *line)
 	     modifiers |= ShiftMask;
 	     break;
 	  case 'A':
-	     modifiers |= Mod1Mask;
-	     break;
 	  case '1':
 	     modifiers |= Mod1Mask;
 	     break;
@@ -557,6 +555,7 @@ ActionDecode(const char *line)
 	  case '3':
 	     modifiers |= Mod3Mask;
 	     break;
+	  case 'W':
 	  case '4':
 	     modifiers |= Mod4Mask;
 	     break;
