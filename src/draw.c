npf/e16

commit 0c16916258bde5de8874b96f6539be6e6b2ab2cf

Enable setting shape on multiple windows.
SVN revision: 81864

src/draw.c
@@ -175,11 +175,11 @@ do_draw_semi_solid(Drawable dr, GC gc,
 #endif /* MR_ENABLE_STIPPLED */
 
 typedef struct {
-   EWin               *ewin;
    Window              root;
    GC                  gc;
    int                 xo, yo, wo, ho;
    int                 bl, br, bt, bb;
+   ShapeWin           *shwin;
 #if MR_ENABLE_TRANSLUCENT
    PixImg             *root_pi;
    PixImg             *ewin_pi;
@@ -188,26 +188,26 @@ typedef struct {
 } ShapeData;
 
 static void
-_ShapeDrawNograb_tech_box(ShapeData * psd, int md, int firstlast,
+_ShapeDrawNograb_tech_box(EWin * ewin, int md, int firstlast,
 			  int xn, int yn, int wn, int hn, int seqno)
 {
-   static ShapeWin    *shape_win = NULL;
+   ShapeData          *psd = (ShapeData *) ewin->shape_data;
 
-   if (firstlast == 0 && !shape_win)
-      shape_win = ShapewinCreate(md);
-   if (!shape_win)
+   if (firstlast == 0 && !psd->shwin)
+      psd->shwin = ShapewinCreate(md);
+   if (!psd->shwin)
       return;
 
-   ShapewinShapeSet(shape_win, md, xn, yn, wn, hn, psd->bl, psd->br, psd->bt,
+   ShapewinShapeSet(psd->shwin, md, xn, yn, wn, hn, psd->bl, psd->br, psd->bt,
 		    psd->bb, seqno);
-   EoMap(shape_win, 0);
+   EoMap(psd->shwin, 0);
 
-   CoordsShow(psd->ewin);
+   CoordsShow(ewin);
 
    if (firstlast == 2)
      {
-	ShapewinDestroy(shape_win);
-	shape_win = NULL;
+	ShapewinDestroy(psd->shwin);
+	psd->shwin = NULL;
      }
 }
 
@@ -225,9 +225,10 @@ static DrawFunc    *const draw_functions[] = {
 };
 
 static void
-_ShapeDrawNontranslucent(ShapeData * psd, int md, int firstlast,
+_ShapeDrawNontranslucent(EWin * ewin, int md, int firstlast,
 			 int xn, int yn, int wn, int hn)
 {
+   ShapeData          *psd = (ShapeData *) ewin->shape_data;
    DrawFunc           *drf;
 
    if (firstlast == 0)
@@ -260,7 +261,7 @@ _ShapeDrawNontranslucent(ShapeData * psd, int md, int firstlast,
       drf(psd->root, psd->gc, psd->xo, psd->yo, psd->wo, psd->ho,
 	  psd->bl, psd->br, psd->bt, psd->bb);
 
-   CoordsShow(psd->ewin);
+   CoordsShow(ewin);
 
    if (firstlast < 2)
       drf(psd->root, psd->gc, xn, yn, wn, hn,
@@ -275,9 +276,10 @@ _ShapeDrawNontranslucent(ShapeData * psd, int md, int firstlast,
 
 #if MR_ENABLE_TRANSLUCENT
 static void
-_ShapeDrawTranslucent(ShapeData * psd, int md __UNUSED__, int firstlast,
+_ShapeDrawTranslucent(EWin * ewin, int md __UNUSED__, int firstlast,
 		      int xn, int yn, int wn, int hn)
 {
+   ShapeData          *psd = (ShapeData *) ewin->shape_data;
    XGCValues           gcv;
    int                 dx, dy, adx, ady;
    int                 xo, yo;
@@ -286,8 +288,8 @@ _ShapeDrawTranslucent(ShapeData * psd, int md __UNUSED__, int firstlast,
    yo = psd->yo;
 
    /* Using frame window size here */
-   wn = EoGetW(psd->ewin);
-   hn = EoGetH(psd->ewin);
+   wn = EoGetW(ewin);
+   hn = EoGetH(ewin);
 
    switch (firstlast)
      {
@@ -308,11 +310,11 @@ _ShapeDrawTranslucent(ShapeData * psd, int md __UNUSED__, int firstlast,
 	     goto do_cleanup;
 	  }
 
-	if (EoGetWin(psd->ewin)->num_rect > 0)
+	if (EoGetWin(ewin)->num_rect > 0)
 	  {
 	     Pixmap              mask;
 
-	     mask = EWindowGetShapePixmapInverted(EoGetWin(psd->ewin));
+	     mask = EWindowGetShapePixmapInverted(EoGetWin(ewin));
 	     PixImgSetMask(psd->draw_pi, mask, 0, 0);
 	  }
 
@@ -355,7 +357,7 @@ _ShapeDrawTranslucent(ShapeData * psd, int md __UNUSED__, int firstlast,
 	     PixImgBlend(psd->root_pi, psd->ewin_pi, psd->draw_pi, psd->root,
 			 xn, yn, wn, hn);
 	  }
-	if (EoGetWin(psd->ewin)->num_rect > 0)
+	if (EoGetWin(ewin)->num_rect > 0)
 	  {
 	     PixImgSetMask(psd->draw_pi, 1, xn, yn);
 	     PixImgPaste11(psd->root_pi, psd->draw_pi, xn, yn, wn, hn);
@@ -397,8 +399,7 @@ void
 DrawEwinShape(EWin * ewin, int md, int x, int y, int w, int h,
 	      int firstlast, int seqno)
 {
-   static ShapeData    sd, *const psd = &sd;
-   Window              root = WinGetXwin(VROOT);
+   ShapeData          *psd;
    int                 dx, dy;
 
    /* Quit if no change */
@@ -420,9 +421,16 @@ DrawEwinShape(EWin * ewin, int md, int x, int y, int w, int h,
      {
 	EwinShapeSet(ewin);
 
-	psd->ewin = ewin;
-	psd->root = root;
+	psd = ECALLOC(ShapeData, 1);
+	ewin->shape_data = psd;
+	if (!psd)
+	   goto done;
+	psd->root = WinGetXwin(VROOT);
+	EwinBorderGetSize(ewin, &psd->bl, &psd->br, &psd->bt, &psd->bb);
      }
+   psd = (ShapeData *) ewin->shape_data;
+   if (!psd)
+      goto done;
 
    dx = EoGetX(EoGetDesk(ewin));
    dy = EoGetY(EoGetDesk(ewin));
@@ -442,12 +450,10 @@ DrawEwinShape(EWin * ewin, int md, int x, int y, int w, int h,
 	h = ewin->shape_h;
      }
 
-   EwinBorderGetSize(ewin, &psd->bl, &psd->br, &psd->bt, &psd->bb);
-
    if (((md <= MR_BOX) || (md == MR_TECH_OPAQUE)) &&
        Conf.movres.avoid_server_grab)
      {
-	_ShapeDrawNograb_tech_box(psd, md, firstlast, x, y, w, h, seqno);
+	_ShapeDrawNograb_tech_box(ewin, md, firstlast, x, y, w, h, seqno);
 	goto done;
      }
 
@@ -460,11 +466,11 @@ DrawEwinShape(EWin * ewin, int md, int x, int y, int w, int h,
      case MR_SHADED:
      case MR_SEMI_SOLID:
 #endif
-	_ShapeDrawNontranslucent(psd, md, firstlast, x, y, w, h);
+	_ShapeDrawNontranslucent(ewin, md, firstlast, x, y, w, h);
 	break;
 #if MR_ENABLE_TRANSLUCENT
      case MR_TRANSLUCENT:
-	_ShapeDrawTranslucent(psd, md, firstlast, x, y, w, h);
+	_ShapeDrawTranslucent(ewin, md, firstlast, x, y, w, h);
 	CoordsShow(ewin);
 	break;
 #endif
@@ -485,8 +491,25 @@ DrawEwinShape(EWin * ewin, int md, int x, int y, int w, int h,
 	ewin->req_x = ewin->shape_x;
 	ewin->req_y = ewin->shape_y;
 	if (firstlast == 2)
-	   CoordsHide();
+	  {
+	     CoordsHide();
+	     Efree(ewin->shape_data);
+	     ewin->shape_data = NULL;
+	  }
      }
+}
+
+void
+DrawEwinShapeEnd(EWin * ewin)
+{
+   ShapeData          *psd = (ShapeData *) ewin->shape_data;
+
+   if (!psd)
+      return;
+   if (psd->shwin)
+      ShapewinDestroy(psd->shwin);
+   Efree(psd);
+   ewin->shape_data = NULL;
 }
 
 int

src/ewins.h
@@ -266,6 +266,8 @@ struct _ewin {
    const EWinOps      *ops;
 
    Timer              *timer;	/* Autoshade timer */
+
+   void               *shape_data;	/* Shape drawing data hook */
 };
 
 #define EWIN_STATE_NEW          0	/* New */
@@ -327,6 +329,7 @@ void                DockIt(EWin * ewin);
 /* draw.c */
 void                DrawEwinShape(EWin * ewin, int md, int x, int y, int w,
 				  int h, int firstlast, int seqno);
+void                DrawEwinShapeEnd(EWin * ewin);
 int                 DrawEwinShapeNeedsGrab(int mode);
 
 /* ewins.c */

src/ewins.c
@@ -1051,6 +1051,7 @@ EwinUnmap1(EWin * ewin)
    Zoom(ewin, 0);
 
    MoveResizeEnd(ewin);
+   DrawEwinShapeEnd(ewin);
 }
 
 static void

supersonic-xserver/ssX-e16

commit aae5938e97496793c6ba950454eae7705fa75c0f

Fix technical/shapewin move mode with window groups
The technical etc. move modes using a shaped window ("avoid server grab"
mode) only properly shaped the shape window of the first group member.

Broken since 0c16916258bde5de8874b96f6539be6e6b2ab2cf - v1.0.12.

This commit changed the mode of operation from one shape window in total
to one per group member, i.e. shapes should no longer be combined.

..and drop useless fl variable in _MoveResizeMoveResume().

src/draw.c
@@ -129,7 +129,7 @@ typedef struct {
 
 static void
 _ShapeDrawNograb_tech_box(EWin * ewin, int md, int firstlast,
-			  int xn, int yn, int wn, int hn, int seqno)
+			  int xn, int yn, int wn, int hn)
 {
    ShapeData          *psd = (ShapeData *) ewin->shape_data;
 
@@ -138,8 +138,8 @@ _ShapeDrawNograb_tech_box(EWin * ewin, int md, int firstlast,
    if (!psd->shwin)
       return;
 
-   ShapewinShapeSet(psd->shwin, md, xn, yn, wn, hn, psd->bl, psd->br, psd->bt,
-		    psd->bb, seqno);
+   ShapewinShapeSet(psd->shwin, md, xn, yn, wn, hn,
+		    psd->bl, psd->br, psd->bt, psd->bb);
    EoMap(psd->shwin, 0);
 
    CoordsShow(ewin);
@@ -201,8 +201,7 @@ _ShapeDrawNontranslucent(EWin * ewin, int md, int firstlast,
 }
 
 void
-DrawEwinShape(EWin * ewin, int md, int x, int y, int w, int h,
-	      int firstlast, int seqno)
+DrawEwinShape(EWin * ewin, int md, int x, int y, int w, int h, int firstlast)
 {
    ShapeData          *psd;
    int                 dx, dy;
@@ -262,7 +261,7 @@ DrawEwinShape(EWin * ewin, int md, int x, int y, int w, int h,
    if (((md <= MR_BOX) || (md == MR_TECH_OPAQUE)) &&
        Conf.movres.avoid_server_grab)
      {
-	_ShapeDrawNograb_tech_box(ewin, md, firstlast, x, y, w, h, seqno);
+	_ShapeDrawNograb_tech_box(ewin, md, firstlast, x, y, w, h);
 	goto done;
      }
 

src/ewins.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2004-2019 Kim Woelders
+ * Copyright (C) 2004-2020 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -343,7 +343,7 @@ void                DockIt(EWin * ewin);
 
 /* draw.c */
 void                DrawEwinShape(EWin * ewin, int md, int x, int y, int w,
-				  int h, int firstlast, int seqno);
+				  int h, int firstlast);
 void                DrawEwinShapeEnd(EWin * ewin);
 int                 DrawEwinShapeNeedsGrab(int mode);
 

src/moveresize.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2004-2015 Kim Woelders
+ * Copyright (C) 2004-2020 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -163,7 +163,7 @@ _MoveResizeMoveEnd(EWin * ewin)
 	for (i = 0; i < num; i++)
 	   DrawEwinShape(gwins[i], Mode_mr.mode,
 			 gwins[i]->shape_x, gwins[i]->shape_y,
-			 gwins[i]->client.w, gwins[i]->client.h, 2, i);
+			 gwins[i]->client.w, gwins[i]->client.h, 2);
      }
    Mode.mode = MODE_NONE;
 
@@ -226,7 +226,7 @@ _MoveResizeMoveSuspend(void)
 	  {
 	     ewin = lst[i];
 	     DrawEwinShape(ewin, Mode_mr.mode, ewin->shape_x,
-			   ewin->shape_y, ewin->client.w, ewin->client.h, 3, i);
+			   ewin->shape_y, ewin->client.w, ewin->client.h, 3);
 	  }
 	Efree(lst);
 
@@ -239,7 +239,7 @@ _MoveResizeMoveResume(void)
 {
    EWin               *ewin, **lst;
    int                 i, num;
-   int                 x, y, fl, dx, dy;
+   int                 x, y, dx, dy;
 
    ewin = Mode_mr.ewin;
    if (!ewin)
@@ -247,12 +247,8 @@ _MoveResizeMoveResume(void)
 
    GrabPointerSet(Mode_mr.events, ECSR_ACT_MOVE, 1);
 
-   fl = 0;
    if (Mode.mode == MODE_MOVE_PENDING)
-     {
-	Mode.mode = MODE_MOVE;
-	fl = 0;			/* This is the first time we draw it */
-     }
+      Mode.mode = MODE_MOVE;
 
    if (Mode_mr.grab_server)
       EGrabServer();
@@ -275,7 +271,7 @@ _MoveResizeMoveResume(void)
 	x = ewin->shape_x + dx;
 	y = ewin->shape_y + dy;
 	DrawEwinShape(ewin, Mode_mr.mode, x, y,
-		      ewin->client.w, ewin->client.h, fl, i);
+		      ewin->client.w, ewin->client.h, 0);
      }
    Efree(lst);
 }
@@ -427,7 +423,7 @@ MoveResizeResizeStart(EWin * ewin, int kbd, int hv)
    EwinShapeSet(ewin);
    ewin->state.show_coords = 1;
    DrawEwinShape(ewin, Conf.movres.mode_resize, EoGetX(ewin), EoGetY(ewin),
-		 ewin->client.w, ewin->client.h, 0, 0);
+		 ewin->client.w, ewin->client.h, 0);
 }
 
 static void
@@ -449,7 +445,7 @@ _MoveResizeResizeEnd(EWin * ewin)
 
    ewin->state.show_coords = 0;
    DrawEwinShape(ewin, Conf.movres.mode_resize, ewin->shape_x, ewin->shape_y,
-		 ewin->shape_w, ewin->shape_h, 2, 0);
+		 ewin->shape_w, ewin->shape_h, 2);
 
    if ((Mode_mr.mode == MR_OPAQUE) || (Mode_mr.mode == MR_TECH_OPAQUE))
      {
@@ -513,7 +509,7 @@ _MoveResizeMoveHandleMotion(void)
 	  {
 	     ewin1 = gwins[i];
 	     DrawEwinShape(ewin1, Mode_mr.mode, EoGetX(ewin1), EoGetY(ewin1),
-			   ewin1->client.w, ewin1->client.h, 0, i);
+			   ewin1->client.w, ewin1->client.h, 0);
 	     if (Conf.movres.mode_move == MR_OPAQUE)
 		Mode_mr.mode = MR_OPAQUE;
 	  }
@@ -649,7 +645,7 @@ _MoveResizeMoveHandleMotion(void)
 	/* draw the new position of the window */
 	DrawEwinShape(ewin1, Mode_mr.mode,
 		      ewin1->shape_x + ndx, ewin1->shape_y + ndy,
-		      ewin1->client.w, ewin1->client.h, 1, i);
+		      ewin1->client.w, ewin1->client.h, 1);
 
 	/* if we didnt jump the window after a resist at the edge */
 	/* reset the requested x to be the prev. requested + delta */
@@ -753,7 +749,7 @@ _MoveResizeResizeHandleMotion(void)
 	break;
      }
 
-   DrawEwinShape(ewin, Conf.movres.mode_resize, x, y, w, h, 1, 0);
+   DrawEwinShape(ewin, Conf.movres.mode_resize, x, y, w, h, 1);
 }
 
 static void

src/shapewin.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2015 Kim Woelders
+ * Copyright (C) 2007-2020 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -109,7 +109,7 @@ ShapewinCreate(int md)
 
 void
 ShapewinShapeSet(ShapeWin * sw, int md, int x, int y, int w, int h,
-		 int bl, int br, int bt, int bb, int seqno)
+		 int bl, int br, int bt, int bb)
 {
    int                 w2, h2;
 
@@ -123,10 +123,8 @@ ShapewinShapeSet(ShapeWin * sw, int md, int x, int y, int w, int h,
 		       0, 0, WinGetW(VROOT), WinGetH(VROOT));
 	XSetForeground(disp, sw->gc, 1);
 	do_draw_technical(sw->mask, sw->gc, x, y, w, h, bl, br, bt, bb);
-	if (seqno == 0)
-	   EShapeSetMask(EoGetWin(sw), 0, 0, sw->mask);
-	else
-	   EShapeUnionMask(EoGetWin(sw), 0, 0, sw->mask);
+
+	EShapeSetMask(EoGetWin(sw), 0, 0, sw->mask);
      }
    else
      {
@@ -137,10 +135,7 @@ ShapewinShapeSet(ShapeWin * sw, int md, int x, int y, int w, int h,
 	h = (h > 5) ? h - 2 : 3;
 	_SHAPE_SET_RECT((&rl[4]), x + bl + 1, y + bt + 1, w, h);
 
-	if (seqno == 0)
-	   EShapeSetRects(EoGetWin(sw), 0, 0, rl, 8);
-	else
-	   EShapeUnionRects(EoGetWin(sw), 0, 0, rl, 8);
+	EShapeSetRects(EoGetWin(sw), 0, 0, rl, 8);
      }
    EoShapeUpdate(sw, 0);
 }

src/shapewin.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Kim Woelders
+ * Copyright (C) 2014-2020 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -38,8 +38,7 @@ struct _ShapeWin {
 ShapeWin           *ShapewinCreate(int md);
 void                ShapewinDestroy(ShapeWin * sw);
 void                ShapewinShapeSet(ShapeWin * sw, int md, int x, int y, int w,
-				     int h, int bl, int br, int bt, int bb,
-				     int seqno);
+				     int h, int bl, int br, int bt, int bb);
 
 void                do_draw_technical(EX_Drawable dr, GC gc,
 				      int a, int b, int c, int d, int bl,

src/slide.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2017 Kim Woelders
+ * Copyright (C) 2013-2020 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -102,7 +102,7 @@ _EwinSlideSizeTo(EObj * eo, int remaining, void *state)
    if (p->mode == MR_OPAQUE)
       EwinMoveResize(ewin, x, y, w, h, MRF_KEEP_MAXIMIZED);
    else
-      DrawEwinShape(ewin, p->mode, x, y, w, h, p->firstlast, 0);
+      DrawEwinShape(ewin, p->mode, x, y, w, h, p->firstlast);
    if (p->firstlast == 0)
       p->firstlast = 1;
 
@@ -111,7 +111,7 @@ _EwinSlideSizeTo(EObj * eo, int remaining, void *state)
 	ewin->state.sliding = 0;
 	if (p->mode != MR_OPAQUE)
 	   DrawEwinShape(ewin, p->mode, p->tx, p->ty,
-			 ewin->client.w, ewin->client.h, 2, 0);
+			 ewin->client.w, ewin->client.h, 2);
 	EwinMove(ewin, p->tx, p->ty, MRF_NOCHECK_ONSCREEN | MRF_KEEP_MAXIMIZED);
 	if (p->flags & SLIDE_WARP)
 	  {

src/warp.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2004-2018 Kim Woelders
+ * Copyright (C) 2004-2020 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -70,7 +70,7 @@ WarpShapeDraw(EWin * ewin)
 
    EwinBorderGetSize(ewin, &bl, &br, &bt, &bb);
    ShapewinShapeSet(shape_win, MR_BOX, EoGetX(ewin), EoGetY(ewin),
-		    ewin->client.w, ewin->client.h, bl, br, bt, bb, 0);
+		    ewin->client.w, ewin->client.h, bl, br, bt, bb);
    EoMap(shape_win, 0);
 }
 