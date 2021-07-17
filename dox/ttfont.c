4f9913301e6ebe299b214ec88f278be091b83c04
Kim Woelders <kim@woelders.dk>
2021-07-17 16:24:23 +0200

    edox: Avoid LTO null pointer warning

--- dox/ttfont.c
+++ dox/ttfont.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2010-2014 Kim Woelders
+ * Copyright (C) 2010-2021 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -115,9 +115,6 @@ Efont_extents(Efont * f, const char *text, int *font_ascent_return,
 	      int *lbearing_return __UNUSED__, int *rbearing_return __UNUSED__)
 {
    int                 w, h;
-
-   if (!f)
-      return;
 
    imlib_context_set_font(f->face);
    imlib_get_text_advance(text, &w, &h);
