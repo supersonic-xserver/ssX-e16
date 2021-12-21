supersonic-xserver/ssX-e16

commit ebe82a7bbba9acd29375e7afb271275b8e26706f

Focus: Fix pointer focus on new windows if pointer is in window
In certain situations new windows would not be focused.

src/focus.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2004-2020 Kim Woelders
+ * Copyright (C) 2004-2021 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -106,6 +106,42 @@ FocusEwinValid(EWin * ewin, int want_on_screen, int click, int want_visible)
        want_on_screen, EwinIsOnScreen(ewin), click, ewin->props.focusclick,
        want_visible, ewin->state.visibility, ok);
 #endif
+   return ok;
+}
+
+/*
+ * Return !0 if new ewin should be focused.
+ */
+static int
+FocusEwinValidNew(EWin * ewin)
+{
+   int                 ok = 0;
+
+   if (Conf.focus.all_new_windows_get_focus)
+      ok = 1;
+
+   else if (Conf.focus.new_windows_get_focus_if_group_focused &&
+	    Mode.focuswin &&
+	    EwinGetWindowGroup(ewin) == EwinGetWindowGroup(Mode.focuswin))
+      ok = 1;
+
+   else if (EwinIsTransient(ewin))
+     {
+	if (Conf.focus.new_transients_get_focus)
+	  {
+	     ok = 1;
+	  }
+	else if (Conf.focus.new_transients_get_focus_if_group_focused &&
+		 Mode.focuswin)
+	  {
+	     if ((EwinGetTransientFor(ewin) ==
+		  EwinGetClientXwin(Mode.focuswin)) ||
+		 (EwinGetWindowGroup(ewin) ==
+		  EwinGetWindowGroup(Mode.focuswin)))
+		ok = 1;
+	  }
+     }
+
    return ok;
 }
 
@@ -309,7 +345,6 @@ ClickGrabsUpdate(void)
 static void
 doFocusToEwin(EWin * ewin, int why)
 {
-   int                 do_focus = 0;
    int                 do_raise = 0, do_warp = 0;
 
    if (focus_inhibit)
@@ -374,38 +409,14 @@ doFocusToEwin(EWin * ewin, int why)
 	if (ewin->props.focus_when_mapped)
 	   goto check_focus_new;
 
-	if (Conf.focus.all_new_windows_get_focus)
-	   goto check_focus_new;
-
-	if (Conf.focus.new_windows_get_focus_if_group_focused &&
-	    Mode.focuswin &&
-	    EwinGetWindowGroup(ewin) == EwinGetWindowGroup(Mode.focuswin))
-	   goto check_focus_new;
-
-	if (Conf.focus.mode != MODE_FOCUS_CLICK && ewin == Mode.mouse_over_ewin)
-	   goto check_focus_new;
-
-	if (EwinIsTransient(ewin))
+	if (FocusEwinValidNew(ewin))
 	  {
-	     if (Conf.focus.new_transients_get_focus)
-	       {
-		  do_focus = 1;
-	       }
-	     else if (Conf.focus.new_transients_get_focus_if_group_focused &&
-		      Mode.focuswin)
-	       {
-		  if ((EwinGetTransientFor(ewin) ==
-		       EwinGetClientXwin(Mode.focuswin)) ||
-		      (EwinGetWindowGroup(ewin) ==
-		       EwinGetWindowGroup(Mode.focuswin)))
-		     do_focus = 1;
-	       }
-
-	     if (!do_focus)
-		return;
 	     DeskGotoByEwin(ewin, 0);
 	     goto check_focus_new;
 	  }
+
+	if (Conf.focus.mode != MODE_FOCUS_CLICK && ewin == Mode.mouse_over_ewin)
+	   goto check_focus_new;
 
 	return;
 
@@ -503,6 +514,8 @@ FocusToEWin(EWin * ewin, int why)
    switch (why)
      {
      case FOCUS_EWIN_NEW:
+	if (!FocusEwinValidNew(ewin))
+	   break;
 	if (!FocusEwinValid(ewin, 0, 0, 0))
 	   break;
 	focus_pending_new = ewin;

commit 9c6cd28436d929d4fb0ee6a8ea40f70caf697359

Do focus windows to be manually placed
Broken by ebe82a7bbba9acd29375e7afb271275b8e26706f, see
https://phab.enlightenment.org/T8973

src/E.h
@@ -376,7 +376,7 @@ typedef struct {
    } move;
    struct {
       signed char         enable_features;
-      char                doing_manual;
+      EWin               *doing_manual;
    } place;
    struct {
       int                 w_old;

src/ewins.c
@@ -983,7 +983,7 @@ AddToFamily(EWin * ewin, EX_Window xwin, XWindowAttributes * pxwa, int startup)
 	EoSetFloating(ewin, 1);	/* Causes reparenting to root */
 	EwinOpFloatAt(ewin, OPSRC_USER, x, y);
 	EwinShow(ewin);
-	Mode.place.doing_manual = 1;
+	Mode.place.doing_manual = ewin;
 	MoveResizeMoveStart(ewin, 0, 0, 0);
      }
    else if (doslide)

src/focus.c
@@ -129,6 +129,9 @@ FocusEwinValidNew(EWin * ewin)
    if (Conf.focus.all_new_windows_get_focus)
       ok = 1;
 
+   else if (Mode.place.doing_manual == ewin)
+      ok = 1;
+
    else if (Conf.focus.new_windows_get_focus_if_group_focused &&
 	    Mode.focuswin &&
 	    EwinGetWindowGroup(ewin) == EwinGetWindowGroup(Mode.focuswin))
@@ -415,9 +418,6 @@ doFocusToEwin(EWin * ewin, int why)
      case FOCUS_EWIN_NEW:
 	if (!ewin)
 	   return;
-
-	if (Mode.place.doing_manual)
-	   goto check_focus_new;
 
 	if (ewin->props.focus_when_mapped)
 	   goto check_focus_new;

src/moveresize.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2004-2020 Kim Woelders
+ * Copyright (C) 2004-2021 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -193,7 +193,7 @@ _MoveResizeMoveEnd(EWin * ewin)
  done:
    Mode.mode = MODE_NONE;
    Mode.move.swap = 0;
-   Mode.place.doing_manual = 0;
+   Mode.place.doing_manual = NULL;
    Mode_mr.ewin = NULL;
 
    if (Mode_mr.grab_server)