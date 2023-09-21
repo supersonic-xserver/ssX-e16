supersonic-xserver/ssX-e16

commit 3c42724f969ba553a63ed8d457aebdb95d3b888b

Dialog layout adjustments
In general left-align text labels.

src/groups.c
@@ -743,8 +743,6 @@ _DlgFillGroupChoose(Dialog * d, DItem * table, void *data)
    DialogItemTableSetOptions(table, 2, 0, 0, 0);
 
    di = DialogAddItem(table, DITEM_TEXT);
-   DialogItemSetColSpan(di, 2);
-   DialogItemSetAlign(di, 0, 512);
    DialogItemSetText(di, dd->message);
 
    num_groups = dd->group_num;
@@ -926,7 +924,6 @@ _DlgFillGroups(Dialog * d, DItem * table, void *data)
 
    di = DialogAddItem(table, DITEM_TEXT);
    DialogItemSetColSpan(di, 2);
-   DialogItemSetAlign(di, 0, 512);
    DialogItemSetText(di, _("Pick the group to configure:"));
 
    group_member_strings = _GrouplistMemberNames(ewin->groups, ewin->num_groups);
@@ -954,7 +951,6 @@ _DlgFillGroups(Dialog * d, DItem * table, void *data)
 
    di = DialogAddItem(table, DITEM_TEXT);
    DialogItemSetColSpan(di, 2);
-   DialogItemSetAlign(di, 0, 512);
    DialogItemSetText(di, _("The following actions are\n"
 			   "applied to all group members:"));
 
@@ -1047,7 +1043,6 @@ _DlgFillGroupDefaults(Dialog * d, DItem * table, void *data __UNUSED__)
 
    di = DialogAddItem(table, DITEM_TEXT);
    DialogItemSetColSpan(di, 2);
-   DialogItemSetAlign(di, 0, 512);
    DialogItemSetText(di, _("Per-group settings:"));
 
    di = DialogAddItem(table, DITEM_CHECKBUTTON);

src/backgrounds.c
@@ -1980,7 +1980,6 @@ _DlgFillBackground(Dialog * d, DItem * table, void *data)
    DialogItemTableSetOptions(table2, 2, 0, 1, 0);
 
    di = dd->bg_filename = DialogAddItem(table2, DITEM_TEXT);
-   DialogItemSetFill(di, 1, 0);
    BG_DialogSetFileName(dd->bg_filename);
 
    table3 = DialogAddItem(table2, DITEM_TABLE);
@@ -2025,7 +2024,6 @@ _DlgFillBackground(Dialog * d, DItem * table, void *data)
    DialogItemTableSetOptions(table2, 3, 0, 1, 0);
 
    di = DialogAddItem(table2, DITEM_TEXT);
-   DialogItemSetFill(di, 0, 0);
    DialogItemSetAlign(di, 512, 512);
    DialogItemSetText(di,
 		     _("Background\n" "Image\n" "Scaling\n" "and\n"
@@ -2080,12 +2078,10 @@ _DlgFillBackground(Dialog * d, DItem * table, void *data)
 
    di = DialogAddItem(table3, DITEM_TEXT);
    DialogItemSetColSpan(di, 2);
-   DialogItemSetFill(di, 0, 0);
    DialogItemSetAlign(di, 512, 512);
    DialogItemSetText(di, _("BG Colour"));
 
    di = DialogAddItem(table3, DITEM_TEXT);
-   DialogItemSetFill(di, 0, 0);
    DialogItemSetAlign(di, 1024, 512);
    DialogItemSetText(di, _("Red:"));
 
@@ -2096,7 +2092,6 @@ _DlgFillBackground(Dialog * d, DItem * table, void *data)
    DialogItemSliderSetValPtr(di, &dd->bg_r);
 
    di = DialogAddItem(table3, DITEM_TEXT);
-   DialogItemSetFill(di, 0, 0);
    DialogItemSetAlign(di, 1024, 512);
    DialogItemSetText(di, _("Green:"));
 
@@ -2107,7 +2102,6 @@ _DlgFillBackground(Dialog * d, DItem * table, void *data)
    DialogItemSliderSetValPtr(di, &dd->bg_g);
 
    di = DialogAddItem(table3, DITEM_TEXT);
-   DialogItemSetFill(di, 0, 0);
    DialogItemSetAlign(di, 1024, 512);
    DialogItemSetText(di, _("Blue:"));
 
@@ -2196,7 +2190,6 @@ _DlgFillBackground(Dialog * d, DItem * table, void *data)
    DialogAddItem(table, DITEM_SEPARATOR);
 
    di = label = DialogAddItem(table, DITEM_TEXT);
-   DialogItemSetAlign(di, 512, 512);
    Esnprintf(s, sizeof(s), _("Unused backgrounds freed after %2i:%02i:%02i"),
 	     dd->bg_timeout / 3600,
 	     (dd->bg_timeout / 60) - (60 * (dd->bg_timeout / 3600)),

src/container.c
@@ -1545,8 +1545,6 @@ _DlgFillContainer(Dialog * d, DItem * table, void *data)
    DialogItemCheckButtonSetPtr(di, &dd->auto_resize);
 
    di = DialogAddItem(table, DITEM_TEXT);
-   DialogItemSetFill(di, 0, 0);
-   DialogItemSetAlign(di, 0, 512);
    DialogItemSetText(di,
 		     _("Alignment of anchoring when automatically resizing:"));
 
@@ -1559,8 +1557,6 @@ _DlgFillContainer(Dialog * d, DItem * table, void *data)
    DialogAddItem(table, DITEM_SEPARATOR);
 
    label = di = DialogAddItem(table, DITEM_TEXT);
-   DialogItemSetFill(di, 0, 0);
-   DialogItemSetAlign(di, 0, 512);
    Esnprintf(s, sizeof(s), _("Icon size: %2d"), dd->iconsize);
    DialogItemSetText(di, s);
 
@@ -1577,8 +1573,6 @@ _DlgFillContainer(Dialog * d, DItem * table, void *data)
    DialogItemTableSetOptions(table2, 5, 0, 0, 0);
 
    di = DialogAddItem(table2, DITEM_TEXT);
-   DialogItemSetFill(di, 0, 0);
-   DialogItemSetAlign(di, 0, 512);
    DialogItemSetText(di, _("Orientation:"));
 
    radio = di = DialogAddItem(table2, DITEM_RADIOBUTTON);
@@ -1596,8 +1590,6 @@ _DlgFillContainer(Dialog * d, DItem * table, void *data)
    DialogAddItem(table2, DITEM_NONE);
 
    di = DialogAddItem(table2, DITEM_TEXT);
-   DialogItemSetFill(di, 0, 0);
-   DialogItemSetAlign(di, 0, 512);
    DialogItemSetText(di, _("Scrollbar side:"));
 
    radio = di = DialogAddItem(table2, DITEM_RADIOBUTTON);
@@ -1615,8 +1607,6 @@ _DlgFillContainer(Dialog * d, DItem * table, void *data)
    DialogAddItem(table2, DITEM_NONE);
 
    di = DialogAddItem(table2, DITEM_TEXT);
-   DialogItemSetFill(di, 0, 0);
-   DialogItemSetAlign(di, 0, 512);
    DialogItemSetText(di, _("Scrollbar arrows:"));
 
    radio = di = DialogAddItem(table2, DITEM_RADIOBUTTON);
@@ -1652,8 +1642,6 @@ _DlgFillContainer(Dialog * d, DItem * table, void *data)
 	DialogItemTableSetOptions(table2, 4, 0, 1, 0);
 
 	di = DialogAddItem(table2, DITEM_TEXT);
-	DialogItemSetFill(di, 0, 0);
-	DialogItemSetAlign(di, 0, 512);
 	DialogItemSetText(di, _("Animation mode:"));
 
 	radio = di = DialogAddItem(table2, DITEM_RADIOBUTTON);
@@ -1673,8 +1661,6 @@ _DlgFillContainer(Dialog * d, DItem * table, void *data)
 	DialogItemRadioButtonGroupSetValPtr(radio, &dd->anim_mode);
 
 	di = DialogAddItem(table, DITEM_TEXT);
-	DialogItemSetFill(di, 0, 0);
-	DialogItemSetAlign(di, 0, 512);
 	DialogItemSetText(di, _("Animation speed:"));
 
 	di = DialogAddItem(table, DITEM_SLIDER);
@@ -1686,8 +1672,6 @@ _DlgFillContainer(Dialog * d, DItem * table, void *data)
 	DialogAddItem(table, DITEM_SEPARATOR);
 
 	di = DialogAddItem(table, DITEM_TEXT);
-	DialogItemSetFill(di, 0, 0);
-	DialogItemSetAlign(di, 0, 512);
 	DialogItemSetText(di,
 			  _
 			  ("Icon image display policy (if one operation fails, try the next):"));

src/desktops.c
@@ -2544,10 +2544,13 @@ _DlgFillDesks(Dialog * d, DItem * table, void *data __UNUSED__)
 
    di = DialogAddItem(table, DITEM_TEXT);
    DialogItemSetColSpan(di, 2);
+   DialogItemSetAlign(di, 512, 512);
    DialogItemSetText(di, _("Number of virtual desktops:"));
 
    di = dd->desk_text = DialogAddItem(table, DITEM_TEXT);
    DialogItemSetColSpan(di, 2);
+   DialogItemSetAlign(di, 512, 512);
+   DialogItemSetFill(di, 1, 1);
    DialogItemSetText(di, "X");
 
    di = slider = DialogAddItem(table, DITEM_SLIDER);
@@ -2575,7 +2578,6 @@ _DlgFillDesks(Dialog * d, DItem * table, void *data __UNUSED__)
 
    di = DialogAddItem(table, DITEM_TEXT);
    DialogItemSetColSpan(di, 2);
-   DialogItemSetAlign(di, 1024, 512);
    DialogItemSetText(di, _("Desktop Slide speed:"));
 
    di = DialogAddItem(table, DITEM_SLIDER);
@@ -2603,7 +2605,6 @@ _DlgFillDesks(Dialog * d, DItem * table, void *data __UNUSED__)
 
    di = DialogAddItem(table, DITEM_TEXT);
    DialogItemSetColSpan(di, 2);
-   DialogItemSetAlign(di, 0, 512);
    DialogItemSetText(di, _("Drag bar position:"));
 
    radio = di = DialogAddItem(table, DITEM_RADIOBUTTON);
@@ -2734,9 +2735,12 @@ _DlgFillAreas(Dialog * d, DItem * table, void *data __UNUSED__)
 
    di = DialogAddItem(table, DITEM_TEXT);
    DialogItemSetText(di, _("Virtual Desktop size:"));
+   DialogItemSetAlign(di, 512, 512);
 
    di = dd->area_text = DialogAddItem(table, DITEM_TEXT);
    DialogItemSetText(di, "X");
+   DialogItemSetAlign(di, 512, 512);
+   DialogItemSetFill(di, 1, 1);
 
    table2 = DialogAddItem(table, DITEM_TABLE);
    DialogItemTableSetOptions(table2, 2, 0, 0, 0);
@@ -2775,7 +2779,6 @@ _DlgFillAreas(Dialog * d, DItem * table, void *data __UNUSED__)
    DialogAddItem(table, DITEM_SEPARATOR);
 
    di = DialogAddItem(table, DITEM_TEXT);
-   DialogItemSetAlign(di, 0, 512);
    DialogItemSetText(di, _("Edge Flip Mode:"));
 
    radio = di = DialogAddItem(table, DITEM_RADIOBUTTON);

src/dialog.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2004-2020 Kim Woelders
+ * Copyright (C) 2004-2022 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -548,14 +548,15 @@ DialogItemCreate(int type)
       return di;
 
    di->type = type;
-   di->align_h = 512;
+   di->fill_h = 1;
+   di->fill_v = 0;
+   di->align_h = 0;
    di->align_v = 512;
    di->row_span = 1;
    di->col_span = 1;
    di->item.table.num_columns = 1;
 
    DialogItemSetPadding(di, 2, 2, 2, 2);
-   DialogItemSetFill(di, 1, 0);
 
    return di;
 }
@@ -601,6 +602,9 @@ DialogAddItem(DItem * dii, int type)
 	di->item.check_button.onoff_ptr = &(di->item.check_button.onoff);
 	di->item.check_button.orig_w = 10;
 	di->item.check_button.orig_h = 10;
+	break;
+     case DITEM_TEXT:
+	di->fill_h = 0;
 	break;
      case DITEM_TABLE:
 	di->item.table.num_columns = 1;

src/focus.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2004-2021 Kim Woelders
+ * Copyright (C) 2004-2022 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -968,8 +968,6 @@ _DlgFillFocus(Dialog * d, DItem * table, void *data __UNUSED__)
    DialogItemCheckButtonSetPtr(di, &dd->autoraise.enable);
 
    di = DialogAddItem(table, DITEM_TEXT);
-   DialogItemSetFill(di, 0, 0);
-   DialogItemSetAlign(di, 0, 512);
    DialogItemSetText(di, _("Autoraise delay:"));
 
    di = DialogAddItem(table, DITEM_SLIDER);

src/menus.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2004-2021 Kim Woelders
+ * Copyright (C) 2004-2022 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -2013,7 +2013,6 @@ _DlgFillMenus(Dialog * d, DItem * table, void *data __UNUSED__)
    DialogItemCheckButtonSetPtr(di, &dd->warp);
 
    di = label = DialogAddItem(table, DITEM_TEXT);
-   DialogItemSetAlign(di, 0, 512);
 
    di = DialogAddItem(table, DITEM_SLIDER);
    DialogItemSliderSetBounds(di, 12, 48);

src/pager.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2004-2021 Kim Woelders
+ * Copyright (C) 2004-2022 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -1826,8 +1826,6 @@ _DlgFillPagers(Dialog * d, DItem * table, void *data __UNUSED__)
 
    di = DialogAddItem(table, DITEM_TEXT);
    DialogItemSetColSpan(di, 2);
-   DialogItemSetFill(di, 0, 0);
-   DialogItemSetAlign(di, 0, 512);
    DialogItemSetText(di, _("Pager Mode:"));
 
    radio = di = DialogAddItem(table, DITEM_RADIOBUTTON);
@@ -1877,8 +1875,6 @@ _DlgFillPagers(Dialog * d, DItem * table, void *data __UNUSED__)
 
    di = label = DialogAddItem(table, DITEM_TEXT);
    DialogItemSetColSpan(di, 2);
-   DialogItemSetFill(di, 0, 0);
-   DialogItemSetAlign(di, 0, 512);
    Esnprintf(s, sizeof(s), "%s %03i %s", _("Pager scanning speed:"),
 	     dd->pager_scan_speed, _("lines per second"));
    DialogItemSetText(di, s);
@@ -1893,8 +1889,6 @@ _DlgFillPagers(Dialog * d, DItem * table, void *data __UNUSED__)
 
    di = DialogAddItem(table, DITEM_TEXT);
    DialogItemSetColSpan(di, 2);
-   DialogItemSetFill(di, 0, 0);
-   DialogItemSetAlign(di, 0, 512);
    DialogItemSetText(di, _("Mouse button to select and drag windows:"));
 
    radio = di = DialogAddItem(table, DITEM_RADIOBUTTON);
@@ -1918,8 +1912,6 @@ _DlgFillPagers(Dialog * d, DItem * table, void *data __UNUSED__)
 
    di = DialogAddItem(table, DITEM_TEXT);
    DialogItemSetColSpan(di, 2);
-   DialogItemSetFill(di, 0, 0);
-   DialogItemSetAlign(di, 0, 512);
    DialogItemSetText(di, _("Mouse button to select desktops:"));
 
    radio = di = DialogAddItem(table, DITEM_RADIOBUTTON);
@@ -1943,8 +1935,6 @@ _DlgFillPagers(Dialog * d, DItem * table, void *data __UNUSED__)
 
    di = DialogAddItem(table, DITEM_TEXT);
    DialogItemSetColSpan(di, 2);
-   DialogItemSetFill(di, 0, 0);
-   DialogItemSetAlign(di, 0, 512);
    DialogItemSetText(di, _("Mouse button to display pager menu:"));
 
    radio = di = DialogAddItem(table, DITEM_RADIOBUTTON);

commit 8cf668e9c048e45ccfdbe116f240c3a92ba9346e

dialogs: Fix layout in window group selection
ASAN complains, bug might cause segv.

Broken by 3c42724f969ba553a63ed8d457aebdb95d3b888b (in v1.0.25).

Reported by Milan Maljković.

src/groups.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2004-2022 Kim Woelders
+ * Copyright (C) 2004-2023 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -740,7 +740,7 @@ _DlgFillGroupChoose(Dialog * d, DItem * table, void *data)
 
    *dd = *(GroupSelDlgData *) data;
 
-   DialogItemTableSetOptions(table, 2, 0, 0, 0);
+   DialogItemTableSetOptions(table, 1, 0, 0, 0);
 
    di = DialogAddItem(table, DITEM_TEXT);
    DialogItemSetText(di, dd->message);
@@ -756,7 +756,6 @@ _DlgFillGroupChoose(Dialog * d, DItem * table, void *data)
 	di = DialogAddItem(table, DITEM_RADIOBUTTON);
 	if (i == 0)
 	   radio = di;
-	DialogItemSetColSpan(di, 2);
 	DialogItemSetCallback(di, _DlgSelectCbGroupChoose, i, NULL);
 	DialogItemSetText(di, group_member_strings[i]);
 	DialogItemRadioButtonSetFirst(di, radio);