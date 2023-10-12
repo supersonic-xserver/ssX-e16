6a66d7a2f6aded9f2e69d56c56b8ed6d815e2eca
Kim Woelders <kim@woelders.dk>
2023-10-12 09:03:31 +0200

    actions: Add debug in Eexec()

--- src/actions.c
+++ src/actions.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2004-2022 Kim Woelders
+ * Copyright (C) 2004-2023 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -77,6 +77,9 @@ Eexec(const char *cmd)
    _ExecSetupEnv(EXEC_NO_LIBHACK);
 
    lst = StrlistFromString(cmd, ' ', &num);
+
+   if (EDebug(EDBUG_TYPE_EXEC))
+      Eprintf("%s: '%s'\n", __func__, cmd);
 
    execvp(lst[0], lst);
 
